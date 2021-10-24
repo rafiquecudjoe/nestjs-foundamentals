f17bbd45590265e1c302594208d56f92ed7ceaaf
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2021-10-24 06:58:57 +0000

    Added swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,21 @@ async function bootstrap() {
       enableImplicitConversion:true,
     }
   }))
+
+
+  const options = new DocumentBuilder()
+    .setTitle("Ilovecoffee")
+    .setDescription("Coffee Application")
+    .setVersion("1.0")
+    .build()
+   
+  const document = SwaggerModule.createDocument(app, options)
+  
+  SwaggerModule.setup('api',app,document)
+  
+  
+
+
   await app.listen(3000);
 }
 bootstrap();
